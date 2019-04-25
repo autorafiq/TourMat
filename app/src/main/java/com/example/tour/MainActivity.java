196d320cb67124c3ce2f5ddc2ea3328b43eae31b
autorafiq <autorafiq639@gmail.com>
2019-04-25 16:30:31 +0600

    all the error solved

--- app/src/main/java/com/example/tour/MainActivity.java
+++ app/src/main/java/com/example/tour/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.tour;
 
 import android.content.Intent;
+import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -17,23 +18,41 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import java.io.IOException;
+import java.nio.CharBuffer;
+
+import maes.tech.intentanim.CustomIntent;
+
 public class MainActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
+    private FirebaseUser user;
+    private static int SPLASH_TIME_OUT = 3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         mAuth = FirebaseAuth.getInstance();
-        FirebaseUser user = mAuth.getCurrentUser();
+        user = mAuth.getCurrentUser();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                goTo();
+            }
+        },SPLASH_TIME_OUT);
+
+
+    }
+
+    private void goTo() {
         if (user != null) {
             startActivity(new Intent(MainActivity.this, ShowActivity.class));
+            CustomIntent.customType(MainActivity.this,"left-to-right");
         } else {
             startActivity(new Intent(MainActivity.this, SingInActivity.class));
+            CustomIntent.customType(MainActivity.this,"right-to-left");
         }
-
     }
 
 
